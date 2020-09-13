b83894579c0e1227f088c71036c741952163c035
JavierDuran2501 <javi.duran1001@gmail.com>
2020-09-13 09:27:39 -0500

    Se implementaron botones a las pantallas tienda y acerca de para volver al menu principal

--- core/src/mx/itesm/equipo4/wattba/PantallaAcercaDe.java
+++ core/src/mx/itesm/equipo4/wattba/PantallaAcercaDe.java
@@ -1,7 +1,15 @@
 package mx.itesm.equipo4.wattba;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.ImageButton;
+import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
+import com.badlogic.gdx.scenes.scene2d.utils.TextureRegionDrawable;
+import com.badlogic.gdx.utils.Align;
 
 public class PantallaAcercaDe extends Pantalla {
     private final Juego juego;
@@ -9,11 +17,37 @@ public class PantallaAcercaDe extends Pantalla {
     //Fondo de la pantalla
     private Texture texturaFondo;
 
+    //Escena contenedora del boton regresar
+    private Stage escenaAcrkd;
+
     public PantallaAcercaDe(Juego juego) {this.juego = juego;}
 
     @Override
     public void show() {
         texturaFondo = new Texture("Pantallas/Acercade.png");
+        acrkde();
+    }
+
+    private void acrkde(){
+        escenaAcrkd = new Stage(vista);
+        //btnRegresar
+        Texture texturaBtnRegresar = new Texture("btnsMenu/btnRegresar.png");
+        TextureRegionDrawable trdBtnRegresar = new TextureRegionDrawable(new TextureRegion(texturaBtnRegresar));
+        ImageButton btnRegresar = new ImageButton(trdBtnRegresar);
+        btnRegresar.setPosition(0,0);
+
+        escenaAcrkd.addActor(btnRegresar);
+
+        Gdx.input.setInputProcessor(escenaAcrkd);
+
+        //Listener del boton regresar
+        btnRegresar.addListener(new ClickListener(){
+            @Override
+            public void clicked(InputEvent event, float x, float y) {
+                super.clicked(event, x, y);
+                juego.setScreen(new PantallaMenu(juego));
+            }
+        });
     }
 
     @Override
@@ -23,7 +57,10 @@ public class PantallaAcercaDe extends Pantalla {
 
         batch.begin();
         batch.draw(texturaFondo,0,0);
+
         batch.end();
+
+        escenaAcrkd.draw();
     }
 
     @Override
--- core/src/mx/itesm/equipo4/wattba/PantallaTienda.java
+++ core/src/mx/itesm/equipo4/wattba/PantallaTienda.java
@@ -1,7 +1,14 @@
 package mx.itesm.equipo4.wattba;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.ImageButton;
+import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
+import com.badlogic.gdx.scenes.scene2d.utils.TextureRegionDrawable;
 
 public class PantallaTienda extends Pantalla {
     private final Juego juego;
@@ -10,9 +17,35 @@ public class PantallaTienda extends Pantalla {
     //Fondo de pantalla
     private Texture texturaFondo;
 
+    //Escena contenedora de los botones
+    private Stage escenaTnda;
+
     @Override
     public void show() {
         texturaFondo = new Texture("Pantallas/Tienda.png");
+        tnda();
+    }
+
+    private void tnda() {
+        escenaTnda = new Stage(vista);
+        //btnRegresar
+        Texture texturaBtnRegresar = new Texture("btnsMenu/btnRegresar.png");
+        TextureRegionDrawable trdBtnRegresar = new TextureRegionDrawable(new TextureRegion(texturaBtnRegresar));
+        ImageButton btnRegresar = new ImageButton(trdBtnRegresar);
+        btnRegresar.setPosition(0,0);
+
+        escenaTnda.addActor(btnRegresar);
+
+        Gdx.input.setInputProcessor(escenaTnda);
+
+        //Listener del boton regresar
+        btnRegresar.addListener(new ClickListener(){
+            @Override
+            public void clicked(InputEvent event, float x, float y) {
+                super.clicked(event, x, y);
+                juego.setScreen(new PantallaMenu(juego));
+            }
+        });
     }
 
     @Override
@@ -23,6 +56,8 @@ public class PantallaTienda extends Pantalla {
         batch.begin();
         batch.draw(texturaFondo,0,0);
         batch.end();
+
+        escenaTnda.draw();
     }
 
     @Override
